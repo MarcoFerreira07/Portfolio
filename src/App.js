088ca6fdaf60116b4bebebdfc3ab85b15632d860
Marco Ferreira <marcoasf.com@gmail.com>
2022-11-07 14:46:29 -0600

    added react and footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>
+    <h1>Marco Ferreira</h1>
+    <nav> <a href="">About Me</a><a href="">Project</a><a href="">Contact Me</a></nav>
+</header>
+
+
+<main class="page-wrapper">
+    <section class="page-section" id="about">
+        <h2>About Me</h2>
+        <div>
+            <p>Im a student at SMU Coding bootcamp being taught by great minds. Give me a passing grade thanks... Just kidding ... not really...  </p>
+        </div>
+    </section>
+    <section class="page-section" id="work">
+        <h2>Work</h2>
+        <div class="flex-container">
+            <a href="#" class="flex-item work1">
+                {/* <!-- Span were left to complete Descriptions of each project --> */}
+                <div>
+                    <h3>Project 1</h3>
+                    <span></span>
+                </div>
+            </a>
+            <a href="#" class="flex-item work2">
+                <div>
+                    <h3>Project 2</h3>
+                    <span></span>
+                </div>
+            </a>            <a href="#" class="flex-item work3">
+                <div>
+                    <h3>Project 3</h3>
+                    <span></span>
+                </div>
+            </a>            <a href="#" class="flex-item work4">
+                <div>
+                    <h3>Project 4</h3>
+                    <span></span>
+                </div>
+            </a>            <a href="#" class="flex-item work5">
+                <div>
+                    <h3>Project 5</h3>
+                    <span></span>
+                </div>
+            </a>
+        </div>
+    
+
+    </section>
+    <Footer></Footer>
+</main>
+</>
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => {
+  
+  return (
+    <section class="page-section contact" id="contact">
+    <h2>Contact Me</h2>
+    <div>
+        <address>
+            <a href="tel:4692745018">469.274.5018</a>
+            <a href="marcoasf.com@gmail.com">marcoasf.com@gmail.com</a>
+            <a href="https://github.com/MarcoFerreira07">GitHub</a>
+            <a href="https://www.linkedin.com/in/marco-ferreira-a3662616a/">LinkedIn</a>
+
+        </address>
+    </div>
+
+</section>
+  );
+};
+
+export default Footer;
