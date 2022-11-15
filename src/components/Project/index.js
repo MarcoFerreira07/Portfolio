fe79a48c7f6eb4862d187675fe12e7ac0b562c40
Marco Ferreira <marcoasf.com@gmail.com>
2022-11-14 23:52:20 -0600

    final

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,37 +6,37 @@ const Project = () => {
     
     <main class="page-wrapper">
     <section class="page-section" id="work">
-        <h2>Work</h2>
+        <h3>Work</h3>
         <div class="flex-container">
             <a href="#" class="work1">
                 {/* <!-- Span were left to complete Descriptions of each project --> */}
                 <div>
                     <h3>Stock Checker</h3>
-                    <span></span>
+                    <span>The objective of this application allows you to view/favorite current/past information on whatever company you are searching into.</span>
                 </div>
             </a>
-            <a href="#" class="flex-item work2">
+            <a href="#" class="flex-container work2">
                 <div>
-                    <h3>Project 2</h3>
+                    <h3>Dietly</h3>
                     <span></span>
                 </div>
-            </a>            <a href="#" class="flex-item work3">
+            </a>           {/* <a href="#" class="flex-item work3">
                 <div>
                     <h3>Project 3</h3>
                     <span></span>
                 </div>
-            </a>            <a href="#" class="flex-item work4">
+            </a>            <a href="#" class="flex-container work4">
                 <div>
                     <h3>Project 4</h3>
                     <span></span>
                 </div>
-            </a>            <a href="#" class="flex-item work5">
+            </a>            <a href="#" class="flex-container work5">
                 <div>
                     <h3>Project 5</h3>
                     <span></span>
                 </div>
-            </a>
-        </div>
+            </a>*/}
+        </div> 
     
 
     </section>
